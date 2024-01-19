eed120378e27697e2044a214b41702a22b474c7b
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-01-19 11:33:37 +0400

    defiend models for Blog and Category

--- blog/my_blog/models.py
+++ blog/my_blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    publication_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog_images/')
+    categories = models.ManyToManyField(Category)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
